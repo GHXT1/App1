c19d831ce9d343bcc1cd6f53015a99556655ee34
GHXT1 <13540662941@163.com>
2020-04-09 16:22:39 +0800

    汇率改进版（第七周）

--- app/src/main/java/com/swufe/myapplication/RateActivity.java
+++ app/src/main/java/com/swufe/myapplication/RateActivity.java
@@ -1,7 +1,11 @@
 package com.swufe.myapplication;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,13 +17,22 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class RateActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class RateActivity extends AppCompatActivity implements Runnable{
     private final String TAG="Rate";
     private float dollarRate=0.1f;
     private float euroRate=0.2f;
     private float wonRate=0.3f;
     EditText rmb;
     TextView show;
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +41,33 @@ public class RateActivity extends AppCompatActivity {
 
         rmb = findViewById(R.id.rmb);
         show = findViewById(R.id.showOut);
+
+        //获取sharedpreferences里保存的数据（汇率）
+        SharedPreferences sharedPreferences=getSharedPreferences("myrate", Activity.MODE_PRIVATE);
+        //SharedPreferences sharePreferences=PreferenceManager.getDefaultSharedPreferences(this);于上面用法相同
+        dollarRate=sharedPreferences.getFloat("dollar_rate",0.0f);
+        euroRate=sharedPreferences.getFloat("euro_rate",0.0f);
+        wonRate=sharedPreferences.getFloat("won_rate",0.0f);
+
+        Log.i(TAG,"onCreate:sp dollarRtate="+dollarRate);
+        Log.i(TAG,"onCreate:sp euroRate="+euroRate);
+        Log.i(TAG,"onCreate:sp wonRtate="+wonRate);
+
+        //开启子线程
+        Thread t=new Thread(this);
+        t.start();
+
+        handler=new Handler(){
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+                if(msg.what==5){
+                    String str=(String)msg.obj;
+                    Log.i(TAG,"handleMessage:getMessage msg="+str);
+                    show.setText(str);
+                }
+                super.handleMessage(msg);
+            }
+        };//这个分号是对这个语句（handler=new Handler()）结束的标志
     }
 
     public void onClick(View btn) {
@@ -100,8 +140,61 @@ public class RateActivity extends AppCompatActivity {
             Log.i(TAG,"onActivityResult:euroRate="+euroRate);
             Log.i(TAG,"onActivityResult:wonRate="+wonRate);
 
+            //将新设置的汇率写到sp里;
+            SharedPreferences sharedPreferences=getSharedPreferences("myrate", Activity.MODE_PRIVATE);
+            SharedPreferences.Editor editor=sharedPreferences.edit();
+            editor.putFloat("dollar_rate",dollarRate);
+            editor.putFloat("euro_rate",euroRate);
+            editor.putFloat("won_rate",wonRate);
+            editor.commit();
+            Log.i(TAG,"onActivityResult:数据已保存到sharedPreferences");
         }
 
+
+  }
+  public void run(){
+        Log.i(TAG,"run:run()....");
+        for(int i=1;i<3;i++){
+            Log.i(TAG,"run="+i);
+            try{
+                Thread.sleep(2000);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+        //获取Msg对象，用于返回主线程；
+      Message msg=handler.obtainMessage(5);
+        msg.obj="Currency Converter";
+        handler.sendMessage(msg);
+
+
+        //获取网络数据
+      URL url= null;
+      try {
+          url = new URL("http://www.usd-cny.com/icbc.htm");
+          HttpURLConnection http=(HttpURLConnection) url.openConnection();
+          InputStream in=http.getInputStream();
+
+          String html=inputStream25tring(in);
+          Log.i(TAG,"run:html="+html);
+      } catch (MalformedURLException e) {
+          e.printStackTrace();
+      } catch (IOException e) {
+          e.printStackTrace();
+      }
+  }
+  private String inputStream25tring(InputStream inputStream) throws IOException {
+        final int bufferSize=1024;
+        final char[] buffer=new char[bufferSize];
+        final StringBuilder out=new StringBuilder();
+        Reader in=new InputStreamReader(inputStream,"gb2312");
+        for(;;){
+            int rsz=in.read(buffer,0,buffer.length);
+            if (rsz<0)
+                break;
+            out.append(buffer,0,rsz);
+        }
+        return  out.toString();
 
   }
 }
