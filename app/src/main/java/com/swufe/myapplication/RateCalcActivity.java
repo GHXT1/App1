f7b84863cffad051bf6c1f14aea48ec09feb6423
GHXT1 <13540662941@163.com>
2020-04-30 11:32:33 +0800

    汇率改进版（第十周）最新版

--- app/src/main/java/com/swufe/myapplication/RateCalcActivity.java
+++ app/src/main/java/com/swufe/myapplication/RateCalcActivity.java
@@ -1,7 +1,10 @@
 package com.swufe.myapplication;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +13,7 @@ public class RateCalcActivity extends AppCompatActivity {
     float rate=0f;
     String TAG="RateCalc";
     String title="";
+    EditText inp2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +25,28 @@ public class RateCalcActivity extends AppCompatActivity {
         Log.i(TAG, "onCreate:title ="+title);
         Log.i(TAG, "onCreate:rate ="+rate);
         ((TextView)findViewById(R.id.title2)).setText(title);
+        inp2 =findViewById(R.id.inp2);
+        inp2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                TextView show=(TextView)RateCalcActivity.this.findViewById(R.id.show2);
+                if(s.length()>0){
+                    float val=Float.parseFloat(s.toString());
+                    show.setText(val+"RMB==>"+(100/rate*val));
+                }else{
+                    show.setText("");
+                }
+            }
+        });
     }
 }
